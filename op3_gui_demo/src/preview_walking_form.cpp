95b4154d9d5663063a4785f3728b640c322ce16b
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2025-04-01 20:50:01 +0900

    Format

--- op3_gui_demo/src/preview_walking_form.cpp
+++ op3_gui_demo/src/preview_walking_form.cpp
@@ -1,27 +1,25 @@
 /*******************************************************************************
-* Copyright 2017 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright 2017 ROBOTIS CO., LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 /* Author: Kayman Jung */
 
 #include "../include/op3_gui_demo/preview_walking_form.h"
 
-PreviewWalkingForm::PreviewWalkingForm(QWidget *parent) :
-  QWidget(parent),
-  p_walking_ui(new Ui::PreviewWalkingForm),
-  is_updating_(false)
+PreviewWalkingForm::PreviewWalkingForm(QWidget* parent)
+  : QWidget(parent), p_walking_ui(new Ui::PreviewWalkingForm), is_updating_(false)
 {
   p_walking_ui->setupUi(this);
 }
@@ -31,11 +29,11 @@ PreviewWalkingForm::~PreviewWalkingForm()
   delete p_walking_ui;
 }
 
-bool PreviewWalkingForm::init(robotis_op::QNodeOP3 *qnode)
+bool PreviewWalkingForm::init(robotis_op::QNodeOP3* qnode)
 {
   bool result = setQNode(qnode);
 
-  if(result == true)
+  if (result == true)
   {
     qRegisterMetaType<geometry_msgs::Point>("geometry_msgs::Point");
     qRegisterMetaType<geometry_msgs::Pose>("geometry_msgs::Pose");
@@ -155,16 +153,16 @@ void PreviewWalkingForm::on_button_footstep_plan_clicked(bool check)
 {
   geometry_msgs::Pose target_pose;
   getPoseFromMarkerPanel(target_pose);
-//  target_pose.position.x = p_walking_ui->dSpinBox_marker_pos_x->value();
-//  target_pose.position.y = p_walking_ui->dSpinBox_marker_pos_y->value();
-//  target_pose.position.z = p_walking_ui->dSpinBox_marker_pos_z->value();
+  //  target_pose.position.x = p_walking_ui->dSpinBox_marker_pos_x->value();
+  //  target_pose.position.y = p_walking_ui->dSpinBox_marker_pos_y->value();
+  //  target_pose.position.z = p_walking_ui->dSpinBox_marker_pos_z->value();
 
-//  double roll = deg2rad<double>(p_walking_ui->dSpinBox_marker_ori_r->value());
-//  double pitch = deg2rad<double>(p_walking_ui->dSpinBox_marker_ori_p->value());
-//  double yaw = deg2rad<double>(p_walking_ui->dSpinBox_marker_ori_y->value());
+  //  double roll = deg2rad<double>(p_walking_ui->dSpinBox_marker_ori_r->value());
+  //  double pitch = deg2rad<double>(p_walking_ui->dSpinBox_marker_ori_p->value());
+  //  double yaw = deg2rad<double>(p_walking_ui->dSpinBox_marker_ori_y->value());
 
-//  Eigen::Quaterniond orientation = rpy2quaternion(roll, pitch, yaw);
-//  tf::quaternionEigenToMsg(orientation, target_pose.orientation);
+  //  Eigen::Quaterniond orientation = rpy2quaternion(roll, pitch, yaw);
+  //  tf::quaternionEigenToMsg(orientation, target_pose.orientation);
 
   qnode_op3_->makeFootstepUsingPlanner(target_pose);
 }
@@ -181,14 +179,13 @@ void PreviewWalkingForm::on_button_footstep_go_clicked(bool check)
   qnode_op3_->setWalkingFootsteps(step_time);
 
   bool clear_path = p_walking_ui->checkBox_clear_path->isChecked();
-  if(clear_path == true)
+  if (clear_path == true)
     qnode_op3_->clearFootsteps();
 
   bool clear_marker = p_walking_ui->checkBox_clear_marker->isChecked();
-  if(clear_marker == true)
+  if (clear_marker == true)
     clearMarkerPanel();
 }
-
 
 void PreviewWalkingForm::on_dSpinBox_marker_pos_x_valueChanged(double value)
 {
@@ -216,7 +213,7 @@ void PreviewWalkingForm::on_dSpinBox_marker_ori_y_valueChanged(double value)
   updateInteractiveMarker();
 }
 
-void PreviewWalkingForm::sendPWalkingCommand(const std::string &command, bool set_start_foot)
+void PreviewWalkingForm::sendPWalkingCommand(const std::string& command, bool set_start_foot)
 {
   op3_online_walking_module_msgs::FootStepCommand msg;
 
@@ -224,7 +221,7 @@ void PreviewWalkingForm::sendPWalkingCommand(const std::string &command, bool se
   msg.step_num = p_walking_ui->dSpinBox_p_walking_step_num->value();
   msg.step_length = p_walking_ui->dSpinBox_p_walking_step_length->value();
   msg.side_length = p_walking_ui->dSpinBox_p_walking_side_length->value();
-  msg.step_angle = p_walking_ui->dSpinBox_p_walking_step_angle->value()*DEG2RAD;
+  msg.step_angle = p_walking_ui->dSpinBox_p_walking_step_angle->value() * DEG2RAD;
   msg.start_leg = set_start_foot ? p_walking_ui->comboBox_p_walking_start_leg->currentText().toStdString() : "";
 
   msg.command = command;
@@ -232,7 +229,7 @@ void PreviewWalkingForm::sendPWalkingCommand(const std::string &command, bool se
   qnode_op3_->sendFootStepCommandMsg(msg);
 }
 
-bool PreviewWalkingForm::setQNode(robotis_op::QNodeOP3 *qnode)
+bool PreviewWalkingForm::setQNode(robotis_op::QNodeOP3* qnode)
 {
   if (qnode == NULL)
     return false;
@@ -294,7 +291,7 @@ void PreviewWalkingForm::updatePosePanel(const geometry_msgs::Pose pose)
   is_updating_ = false;
 }
 
-void PreviewWalkingForm::getPoseFromMarkerPanel(geometry_msgs::Pose &current)
+void PreviewWalkingForm::getPoseFromMarkerPanel(geometry_msgs::Pose& current)
 {
   // position
   current.position.x = p_walking_ui->dSpinBox_marker_pos_x->value();
@@ -309,7 +306,7 @@ void PreviewWalkingForm::getPoseFromMarkerPanel(geometry_msgs::Pose &current)
   tf::quaternionEigenToMsg(orientation, current.orientation);
 }
 
-void PreviewWalkingForm::setPoseToMarkerPanel(const geometry_msgs::Pose &current)
+void PreviewWalkingForm::setPoseToMarkerPanel(const geometry_msgs::Pose& current)
 {
   // position
   p_walking_ui->dSpinBox_marker_pos_x->setValue(current.position.x);
@@ -324,7 +321,7 @@ void PreviewWalkingForm::setPoseToMarkerPanel(const geometry_msgs::Pose &current
   p_walking_ui->dSpinBox_marker_ori_y->setValue(euler[2]);
 }
 
-void PreviewWalkingForm::getPointFromMarkerPanel(geometry_msgs::Point &current)
+void PreviewWalkingForm::getPointFromMarkerPanel(geometry_msgs::Point& current)
 {
   // position
   current.x = p_walking_ui->dSpinBox_marker_pos_x->value();
@@ -332,7 +329,7 @@ void PreviewWalkingForm::getPointFromMarkerPanel(geometry_msgs::Point &current)
   current.z = p_walking_ui->dSpinBox_marker_pos_z->value();
 }
 
-void PreviewWalkingForm::setPointToMarkerPanel(const geometry_msgs::Point &current)
+void PreviewWalkingForm::setPointToMarkerPanel(const geometry_msgs::Point& current)
 {
   // position
   p_walking_ui->dSpinBox_marker_pos_x->setValue(current.x);
@@ -349,7 +346,7 @@ void PreviewWalkingForm::setPointToMarkerPanel(const geometry_msgs::Point &curre
  ** Implementation [Util]
  *****************************************************************************/
 
-Eigen::Vector3d PreviewWalkingForm::rotation2rpy(const Eigen::MatrixXd &rotation)
+Eigen::Vector3d PreviewWalkingForm::rotation2rpy(const Eigen::MatrixXd& rotation)
 {
   Eigen::Vector3d rpy;
 
@@ -360,19 +357,19 @@ Eigen::Vector3d PreviewWalkingForm::rotation2rpy(const Eigen::MatrixXd &rotation
   return rpy;
 }
 
-Eigen::MatrixXd PreviewWalkingForm::rpy2rotation(const double &roll, const double &pitch, const double &yaw)
+Eigen::MatrixXd PreviewWalkingForm::rpy2rotation(const double& roll, const double& pitch, const double& yaw)
 {
   Eigen::MatrixXd rotation = rotationZ(yaw) * rotationY(pitch) * rotationX(roll);
 
   return rotation;
 }
 
-Eigen::Quaterniond PreviewWalkingForm::rpy2quaternion(const Eigen::Vector3d &euler)
+Eigen::Quaterniond PreviewWalkingForm::rpy2quaternion(const Eigen::Vector3d& euler)
 {
   return rpy2quaternion(euler[0], euler[1], euler[2]);
 }
 
-Eigen::Quaterniond PreviewWalkingForm::rpy2quaternion(const double &roll, const double &pitch, const double &yaw)
+Eigen::Quaterniond PreviewWalkingForm::rpy2quaternion(const double& roll, const double& pitch, const double& yaw)
 {
   Eigen::MatrixXd rotation = rpy2rotation(roll, pitch, yaw);
 
@@ -386,7 +383,7 @@ Eigen::Quaterniond PreviewWalkingForm::rpy2quaternion(const double &roll, const 
   return quaternion;
 }
 
-Eigen::Quaterniond PreviewWalkingForm::rotation2quaternion(const Eigen::MatrixXd &rotation)
+Eigen::Quaterniond PreviewWalkingForm::rotation2quaternion(const Eigen::MatrixXd& rotation)
 {
   Eigen::Matrix3d rotation3d;
 
@@ -398,14 +395,14 @@ Eigen::Quaterniond PreviewWalkingForm::rotation2quaternion(const Eigen::MatrixXd
   return quaternion;
 }
 
-Eigen::Vector3d PreviewWalkingForm::quaternion2rpy(const Eigen::Quaterniond &quaternion)
+Eigen::Vector3d PreviewWalkingForm::quaternion2rpy(const Eigen::Quaterniond& quaternion)
 {
   Eigen::Vector3d rpy = rotation2rpy(quaternion.toRotationMatrix());
 
   return rpy;
 }
 
-Eigen::Vector3d PreviewWalkingForm::quaternion2rpy(const geometry_msgs::Quaternion &quaternion)
+Eigen::Vector3d PreviewWalkingForm::quaternion2rpy(const geometry_msgs::Quaternion& quaternion)
 {
   Eigen::Quaterniond eigen_quaternion;
   tf::quaternionMsgToEigen(quaternion, eigen_quaternion);
@@ -415,14 +412,14 @@ Eigen::Vector3d PreviewWalkingForm::quaternion2rpy(const geometry_msgs::Quaterni
   return rpy;
 }
 
-Eigen::MatrixXd PreviewWalkingForm::quaternion2rotation(const Eigen::Quaterniond &quaternion)
+Eigen::MatrixXd PreviewWalkingForm::quaternion2rotation(const Eigen::Quaterniond& quaternion)
 {
   Eigen::MatrixXd rotation = quaternion.toRotationMatrix();
 
   return rotation;
 }
 
-Eigen::MatrixXd PreviewWalkingForm::rotationX(const double &angle)
+Eigen::MatrixXd PreviewWalkingForm::rotationX(const double& angle)
 {
   Eigen::MatrixXd rotation(3, 3);
 
@@ -431,7 +428,7 @@ Eigen::MatrixXd PreviewWalkingForm::rotationX(const double &angle)
   return rotation;
 }
 
-Eigen::MatrixXd PreviewWalkingForm::rotationY(const double &angle)
+Eigen::MatrixXd PreviewWalkingForm::rotationY(const double& angle)
 {
   Eigen::MatrixXd rotation(3, 3);
 
@@ -440,7 +437,7 @@ Eigen::MatrixXd PreviewWalkingForm::rotationY(const double &angle)
   return rotation;
 }
 
-Eigen::MatrixXd PreviewWalkingForm::rotationZ(const double &angle)
+Eigen::MatrixXd PreviewWalkingForm::rotationZ(const double& angle)
 {
   Eigen::MatrixXd rotation(3, 3);
 
